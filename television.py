9d84f38975a8053b1060ddaf8d5581e7d5135f28
arthur7016 <hernandezart049@gmail.com>
2025-11-21 11:19:57 -0600

    fully updated

--- television.py
+++ television.py
@@ -4,28 +4,60 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
+
+
+
+        '''
+        Sets the defaults for the Television object
+        args: None
+        return: None
+        '''
 
         self.__status = False
         self.__muted = False
         self.__volume = 0
         self.__channel = 0
+        self.__prev_volume = 0
 
 
-    def power(self):
+
+
+    def power(self) -> None:
+
+        '''
+        Turns the television on and off
+        args: None
+        return: None
+        '''
 
         if self.__status:
             self.__status = False
         else:
             self.__status = True
-    def mute(self):
+    def mute(self) -> None:
+
+        '''
+        Mutes the television
+        args: None
+        return: None
+        '''
 
         if self.__status:
             if self.__muted:
                 self.__muted = False
+                self.__volume = self.__prev_volume
             else:
                 self.__muted = True
-    def channel_up(self):
+                self.__prev_volume = self.__volume
+                self.__volume = self.MIN_VOLUME
+    def channel_up(self) -> None:
+
+        '''
+        Changes the channel up
+        args: None
+        return: None
+        '''
 
         if self.__status:
             if self.__channel == self.MAX_CHANNEL:
@@ -33,7 +65,17 @@ class Television:
             else:
                 self.__channel += 1
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+
+
+
+        '''
+        Changes the channel down
+        args: None
+        return: None
+        '''
+
+
 
         if self.__status:
             if self.__channel == self.MIN_CHANNEL:
@@ -41,22 +83,44 @@ class Television:
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+
+        '''
+        Changes the volume up
+        args: None
+        return: None
+        '''
 
         if self.__status:
+            if self.__muted:
+                self.__muted = False
+                self.__volume = self.__prev_volume
             if self.__volume < self.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
 
-
+        '''
+        Changes the volume down
+        args: None
+        return: None
+        '''
         if self.__status:
+            if self.__muted:
+                self.__muted = False
+                self.__volume = self.__prev_volume
             if self.__volume > self.MIN_VOLUME:
                 self.__volume -= 1
 
 
 
 
-    def __str__(self):
+    def __str__(self) -> str:
+
+        '''
+        Returns a string representation of the Television object
+        args: None
+        return: string
+        '''
 
         return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
